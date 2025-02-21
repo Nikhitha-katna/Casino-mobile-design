4db2b9778758ffde71fdc9d840c70f8d6452e1fd
Nikhitha Katna <katnanikhitha@gmail.com>
2025-02-21 08:49:58 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,4 @@
 
-
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { FiMenu, FiX } from 'react-icons/fi'
@@ -17,7 +16,7 @@ function Header() {
 
   return (
     <>
-      <header>
+      <header className="relative z-50">
         <nav className='flex justify-between items-center bg-sky-800 px-4 py-3 md:px-8'>
           {/* logo */}
           <div>
@@ -32,8 +31,9 @@ function Header() {
           </div>
 
           {/* navigation */}
-          <div className={`${menuOpen ? 'flex' : 'hidden'} md:flex flex-col md:flex-row md:items-center md:gap-6 gap-2 text-[18px] font-bold absolute md:static top-16 left-0 w-full bg-sky-800 md:bg-transparent p-4 md:p-0`}>
-            <ul className='flex flex-col  text-[12px] md:flex-row md:ml-5  md:items-center md:justify-around md:text-[18px] md:gap-2 gap-2'>
+          <div className={`${menuOpen ? 'flex' : 'hidden'} md:flex flex-col md:flex-row md:items-center md:gap-6 gap-2 text-[18px] font-bold 
+          absolute md:static top-16 left-0 w-full bg-sky-800 md:bg-transparent p-4 md:p-0 z-50`}>
+            <ul className='flex flex-col text-[12px] md:flex-row md:ml-5 md:items-center md:justify-around md:text-[18px] md:gap-2 gap-2'>
               <li>
                 <NavLink to='livecasino' onClick={closeMenu} className={({ isActive }) => `${isActive ? "bg-sky-400" : "bg-sky-800"} rounded-2xl px-4 py-2.5`}> Live Casino </NavLink>
               </li>
@@ -60,7 +60,7 @@ function Header() {
 
           {/* login button */}
           <div className='hidden md:flex items-center'>
-            <button className='bg-sky-700 font-bold py-2 px-4 rounded-2xl text-white hover:bg-sky-400'>Login </button>
+            <button className='bg-sky-700 font-bold py-2 px-4 rounded-2xl text-white hover:bg-sky-400'>Login  </button>
           </div>
         </nav>
       </header>
@@ -69,3 +69,5 @@ function Header() {
 }
 
 export default Header
+
+
